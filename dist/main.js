f1e370d5ab9f28d73464b061831ab7b8a77273db
MedNoun <mouhasahss@gmail.com>
2022-03-31 20:51:56 +0100

    adding cors

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
+    const corsOptions = {
+        origin: ['http://localhost:4201', 'http://localhost:4200'],
+        optionsSuccessStatus: 200
+    };
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors(corsOptions);
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+	const corsOptions = {
+		origin: ['http://localhost:4201', 'http://localhost:4200'],
+		optionsSuccessStatus: 200
+	}
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
