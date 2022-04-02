8b10c41d67ebb028d8b63252e935c7d869bb782c
MedNoun <mouhasahss@gmail.com>
2022-04-02 14:04:44 +0100

    adding card template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DatafetcherModule } from '../datafetcher/datafetcher.module';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-    declarations: [NavbarComponent],
+    declarations: [NavbarComponent, CardComponent],
     imports: [CommonModule, DatafetcherModule],
     providers: [],
-    exports: [CommonModule, NavbarComponent, DatafetcherModule]
+    exports: [CommonModule, NavbarComponent, DatafetcherModule, CardComponent]
 })
 export class SharedModule {}
