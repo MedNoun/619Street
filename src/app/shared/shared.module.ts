e84096e3e2c3bcd3f3c5f3f55ec2a637cf5be7f4
MedNoun <mouhasahss@gmail.com>
2022-04-12 12:31:33 +0100

    addind design to assets so that you dont search for them

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,21 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { DatafetcherModule } from '../datafetcher/datafetcher.module';
 import { CardComponent } from './components/card/card.component';
 import { Style01Component } from './components/style01/style01.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-    declarations: [NavbarComponent, CardComponent, Style01Component],
+    declarations: [
+        NavbarComponent,
+        CardComponent,
+        Style01Component,
+        FooterComponent
+    ],
     imports: [CommonModule, DatafetcherModule],
     providers: [],
     exports: [
         CommonModule,
         NavbarComponent,
+        FooterComponent,
         DatafetcherModule,
         CardComponent,
         Style01Component
