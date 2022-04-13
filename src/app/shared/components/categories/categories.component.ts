724697b24831ea626acc7ef56de79e137455c9d3
slim <slim.hammami1977@gmail.com>
2022-04-13 23:54:36 +0200

    making categories component dynamic

--- src/app/shared/components/categories/categories.component.ts
+++ src/app/shared/components/categories/categories.component.ts
@@ -3,21 +3,20 @@ import { categories } from './../../../classes/fetchers/categoriespage/categorie
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+    selector: 'app-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
-  public content : categories[]
-  constructor(private api : ApiService) { 
-    api.getCategories().subscribe((value) => {
-      this.content = value.categories
+    public content: categories[];
+    public firstOfContent: categories;
+    constructor(private api: ApiService) {
+        api.getCategories().subscribe((value) => {
+            this.content = value.categories;
+            this.firstOfContent = this.content.shift();
+        });
     }
-  )
-  }
 
-  ngOnInit(): void {
-
-  }
-  
+    ngOnInit(): void {
+    }
 }
