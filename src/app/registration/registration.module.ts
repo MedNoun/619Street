f13b86aa2ba1682db49f7c35bb75f1431e57f947
BorchaniSalma <sborcheni@ieee.org>
2022-04-16 04:10:36 +0200

    some changes

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { SharedModule } from '../shared/shared.module';
 import { RegistrationComponent } from './registration.component';
 
 @NgModule({
     declarations: [RegistrationComponent],
-    imports: [SharedModule],
+    imports: [SharedModule,NavbarComponent],
     exports: [RegistrationComponent]
 })
 export class RegistrationModule {}
