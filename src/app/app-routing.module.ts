2c57d0fcebefcf041b5a9d656c17b45db7da0a9d
MedNoun <mouhasahss@gmail.com>
2022-04-29 11:20:54 +0100

    adding homepage and catalogue resolvers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageResolverService } from './homepage/services/homepageResolver.service';
+import { CatalogueResolverService } from './catalogue/services/catalogueResolver.service';
 import { LoginComponent } from './login/login.component';
 import { ProductViewComponent } from './product/product-view/product-view.component';
 import { ProductResolverService } from './product/services/product-resolver.service';
@@ -10,11 +12,13 @@ import { RegistrationComponent } from './registration/registration.component';
 const routes: Routes = [
     {
         path: '',
-        component: HomepageComponent
+        component: HomepageComponent,
+        resolve: { content: HomepageResolverService }
     },
     {
         path: 'catalogue',
-        component: CatalogueComponent
+        component: CatalogueComponent,
+        resolve: { content: CatalogueResolverService }
     },
     {
         path: 'login',
