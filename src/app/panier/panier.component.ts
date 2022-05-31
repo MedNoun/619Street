d6aa76bb09cd8f50c21526186ae1907a56c3e98a
BorchaniSalma <sborcheni@ieee.org>
2022-05-31 12:27:19 +0200

    test

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { content, item, products } from '../classes/fetchers/panier/content';
 import { ApiService } from '../datafetcher/service/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-panier',
-  templateUrl: './panier.component.html',
-  styleUrls: ['./panier.component.scss']
+    selector: 'app-panier',
+    templateUrl: './panier.component.html',
+    styleUrls: ['./panier.component.scss']
 })
 export class PanierComponent implements OnInit {
-  public content: content;
-  public products: products = new products();
-  public item : item = new item();
+    public content: content;
+    public products: products = new products();
+    public item: item = new item();
 
-    constructor(private api: ApiService) {
+    constructor(
+        private api: ApiService,
+        private readonly activatedRoute: ActivatedRoute
+    ) {
         api.getPanier().subscribe((value) => {
             this.content = value;
         });
     }
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.activatedRoute.data.subscribe((data: { content }) => {
+            this.content = data.content;
+        });
+    }
 }
